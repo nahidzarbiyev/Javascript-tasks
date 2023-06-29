2d32f6b7fb1131bb87b955335f8d656a658edc84
ichbinnahid <nahid.zarbiyev@gmail.com>
2023-06-29 19:33:12 +0400

    shopping cart js

---
+++ shopping_card/script.js
@@ -0,0 +1,10 @@
+let circle = document.querySelector(".color-option");
+
+circle.addEventListener("click", (e) => {
+  if (e.target.classList.contains("circle")) {
+    circle.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    document.querySelector(".main-images .active").classList.remove('active')
+    document.querySelector(`.main-images .${e.target.id}`).classList.add('active')
+  }
+});
