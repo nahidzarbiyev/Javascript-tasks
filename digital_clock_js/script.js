ab6943b16e12ae0b6b97f5a5b700449fc40f2d93
ichbinnahid <nahid.zarbiyev@gmail.com>
2023-07-09 02:45:44 +0400

    digital clock

---
+++ digital_clock_js/script.js
@@ -0,0 +1,20 @@
+const hour = document.getElementById('hour')
+const minute = document.getElementById('minute')
+const second = document.getElementById('second')
+
+const clock = setInterval(() => {
+    const newDate = new Date();
+    let hr = newDate.getHours()
+    let min = newDate.getMinutes()
+    let sec = newDate.getSeconds()
+    hr = hr.toString().padStart(2,"0")
+    min = min.toString().padStart(2,"0")
+    sec = sec.toString().padStart(2,"0")
+
+    const timeString = `${hr}:${min}:${sec}`
+    // console.log(timeString);
+    hour.innerText = hr;
+    minute.innerText = min;
+    second.innerText = sec;
+
+}, 1000);
